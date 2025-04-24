4f7242c3281f06f311981e9fd6a8d6d2a1c8cca7
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-04-24 07:35:58 +0000

    Data Type Conversion

---
+++ Basics/coversionoperations.js
@@ -0,0 +1,29 @@
+let score = false
+
+console.log(typeof score);
+
+let ValueInNumber = Number(score)
+
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+// "33" = 33
+//"33anc" = NaN
+//"True" = 1
+//"False" = 0
+
+let IsLoggedIn = "Shubham"
+
+let BoolIsLoggedIn = Boolean(IsLoggedIn)
+
+console.log(BoolIsLoggedIn);
+
+// 1=> true  0=> False
+// ""=> false  "Shubham" =>True
+
+let SomeNumber = 39
+
+let StringNumber = String(SomeNumber)
+
+console.log(StringNumber);
+console.log(typeof StringNumber);
