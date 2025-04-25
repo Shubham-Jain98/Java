9479e80304185298ba7d53e4b957925870d1be23
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-04-25 14:37:34 +0000

    Every topic of operations done

--- Basics/coversionoperations.js
+++ Basics/coversionoperations.js
@@ -1,11 +1,11 @@
 let score = false
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let ValueInNumber = Number(score)
 
-console.log(typeof ValueInNumber)
-console.log(ValueInNumber)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
 
 // "33" = 33
 //"33anc" = NaN
@@ -16,7 +16,7 @@ let IsLoggedIn = "Shubham"
 
 let BoolIsLoggedIn = Boolean(IsLoggedIn)
 
-console.log(BoolIsLoggedIn);
+// console.log(BoolIsLoggedIn);
 
 // 1=> true  0=> False
 // ""=> false  "Shubham" =>True
@@ -25,5 +25,32 @@ let SomeNumber = 39
 
 let StringNumber = String(SomeNumber)
 
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// **********************Operations****************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Shubham"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true)
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2= num3 = 2+2
+
+let GameCounter = 100
+++GameCounter;
+console.log(GameCounter);
