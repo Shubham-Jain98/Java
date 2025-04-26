e0dd71fe3589847d2c45a0f75191e17fb71f8fd7
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-04-26 09:30:42 +0000

    Summary of all data types

---
+++ Basics/dataypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const OutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const AnotherId = Symbol('123')
+
+// console.log(id===AnotherId);
+
+const BigNumber = 2323236446585648n
+
+// Reference(Non Primitive)
+
+// Arrays, Objects, Functions
+
+const Actors = ["Salman Khan", "Sharukh Khan", "Shami Kapoor"]
+let mySelf = {
+    name: "shubham",
+    age: 21,
+}
+
+const MyFunction = function(){
+    console.log("Hello");
+    
+}
+
+// console.log(typeof Actors);
