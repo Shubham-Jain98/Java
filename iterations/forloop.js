1a363ef233a90f98c0305da25144bb452da193b8
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-05-03 17:06:44 +0000

    For loop

---
+++ iterations/forloop.js
@@ -0,0 +1,49 @@
+//  for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is a number");
+        
+    }
+    // console.log(element);
+    
+}
+
+// table from 1 to 10 
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value: ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`inner loop value: ${j} with outer loop value ${i}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }   
+}
+
+let myArrays = ["Flash", "Superman", "Batman"]
+
+for (let index = 0; index < myArrays.length; index++) {
+    const element = myArrays[index];
+    // console.log(element);
+}
+
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        // console.log("5 is detected");
+        break
+    }
+    // console.log(`Value of i is ${i}`);
+        
+}
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("5 is detected");
+        continue
+    }
+    console.log(`Value of i is ${i}`);
+        
+}
