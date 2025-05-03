3679dba388bcf41c8bb93301632cda0934b9106d
Shubham-Jain98 <mikeysingh6969@gmail.com>
2025-05-03 17:15:20 +0000

    while and do while loop

---
+++ iterations/while loop.js
@@ -0,0 +1,21 @@
+
+let i = 0
+while (i<= 20) {
+    // console.log(`Value of i is ${i}`);
+    i = i+2
+}
+
+let myArrays = ["Flash", "Superman", "Batman"]
+
+let arr = 0
+while (arr < myArrays.length) {
+    // console.log(`Value is ${myArrays[arr]}`);
+    arr++
+}
+
+let score = 11
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score<= 10);
